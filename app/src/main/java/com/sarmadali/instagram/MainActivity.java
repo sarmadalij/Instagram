6cdd252f79f984975bbbca75921f322c9397a311
Sarmad Ali Jalbani <developer.sarmadali@gmail.com>
2023-08-07 19:17:23 +0500

    toolbar problem solved partially

--- app/src/main/java/com/sarmadali/instagram/MainActivity.java
+++ app/src/main/java/com/sarmadali/instagram/MainActivity.java
@@ -5,17 +5,17 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.sarmadali.instagram.databinding.ActivityMainBinding;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+//    Toolbar toolbar;
 
+   Toolbar toolbar;
 
 
     @Override
@@ -25,20 +25,26 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
 
 
         //hide Action toolbar
 
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
+
+
 
     }
 
